11f7535c4307f087af9af0582b304014578c0e77
Paul Ren <pause.forever@gmail.com>
2022-09-19 16:54:29 +0700

    finish ex2

--- ex-2/app.js
+++ ex-2/app.js
@@ -1,1 +1,15 @@
 // Start coding here
+const likeButton = document.querySelector(".like-button");
+const dislikeButton = document.querySelector(".dislike-button");
+const likeAdjust = document.querySelector(".post-likes");
+
+likeButton.addEventListener("click", () => {
+  let addLike = Number(likeAdjust.textContent);
+  likeAdjust.textContent = addLike + 1;
+});
+dislikeButton.addEventListener("click", () => {
+  if (likeAdjust.textContent > 0) {
+    let addDislike = Number(likeAdjust.textContent);
+    likeAdjust.textContent = addDislike - 1;
+  }
+});

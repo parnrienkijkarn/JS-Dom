729475fbe4cd58b05cec143e6a70888c20c7acd1
Paul Ren <pause.forever@gmail.com>
2022-09-19 15:42:05 +0700

    add answers

--- ex-1/app.js
+++ ex-1/app.js
@@ -1,1 +1,17 @@
 // Start coding here
+const inputMessage = document.getElementById("message-text");
+const enterButton = document.querySelector(".submit-message-button");
+const displayBoard = document.querySelector(".board");
+
+function clickAction() {
+  let message = inputMessage.value;
+  if (message) {
+    let newMessage = document.createElement("h1");
+    newMessage.className = "message";
+    let newInput = document.createTextNode(message);
+    newMessage.appendChild(newInput);
+    displayBoard.appendChild(newMessage);
+  }
+}
+
+enterButton.addEventListener("click", clickAction);
